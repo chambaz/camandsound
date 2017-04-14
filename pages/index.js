aa8daea6e7126ab2920ad27e2044ed421e010334
Adam Chambers <adammchambers@gmail.com>
2017-04-14 12:15:53 -0400

    Fix social component and add new bio block component to inex

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,17 @@ export default () => (
     </Head>
     <Header />
     <Block
-      subTitle='“A masterful debut of innovative hiphop”'
+      subTitle='From Where We Started...'
+      body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum libero arcu, aliquam condimentum nibh ullamcorper ut. Nulla a blandit urna, ut porta sapien. Vestibulum consequat urna sed neque interdum semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum libero arcu, aliquam condimentum nibh ullamcorper ut. Nulla a blandit urna, ut porta sapien. Vestibulum consequat urna sed neque interdum semper.' />
+    <Block
+      subTitle='&ldquo;A masterful debut of innovative hiphop&rdquo;'
       title='A Daydream at Night'
-      body='Available now on all major streaming services' />
+      body='Available now on all major streaming services'
+      embed='<iframe src="https://embed.spotify.com/?uri=spotify%3Aalbum%3A5MoKK3sCGt3QUnR1tds6aS" width="640" height="352" frameborder="0" allowtransparency="true"></iframe>' />
     <Block
       subTitle='The official acoustic music video'
       title='Blissful Ignorance'
-      body='' />
+      embed='<div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/Il0iglZk7IA?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>' />
     <Social />
   </div>
 )
