f119c70608388ce4099f0e1539626e2aeba43e83
Adam Chambers <adammchambers@gmail.com>
2017-09-03 09:40:39 -0400

    Fix meta tag closing in document

--- pages/_document.js
+++ pages/_document.js
@@ -31,12 +31,12 @@ export default class Doc extends Document {
           <meta name='twitter:site' content='@camandsound' />
           <meta name='twitter:image' content='https://camandsound.com/static/img/img-share-small.jpg' />
           <meta name='twitter:creator' content='@camandsound' />
-          <link rel='apple-touch-icon' sizes='180x180' href='/static/favicons/apple-touch-icon.png'>
-          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicons/favicon-32x32.png'>
-          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicons/favicon-16x16.png'>
-          <link rel='manifest' href='/static/favicons/manifest.json'>
-          <link rel='mask-icon' href='/static/favicons/safari-pinned-tab.svg' color='#5bbad5'>
-          <meta name='theme-color' content='#ffffff'>
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/favicons/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicons/favicon-16x16.png' />
+          <link rel='manifest' href='/static/favicons/manifest.json' />
+          <link rel='mask-icon' href='/static/favicons/safari-pinned-tab.svg' color='#5bbad5' />
+          <meta name='theme-color' content='#ffffff' />
           <style>{`
             * {
               margin: 0;
